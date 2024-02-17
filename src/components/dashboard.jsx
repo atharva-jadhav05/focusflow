ac1390870dfb7abfc898b8c892eb0eed57dad719
atharva-jadhav05 <160360862+atharva-jadhav05@users.noreply.github.com>
2024-02-18 04:21:58 +0530

    Update dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,12 +4,11 @@ import { useLocation } from 'react-router-dom';
 const Dashboard = () => {
     const location = useLocation();
     const { state } = location;
-    const access_token = state.credential;
+    const access_token = state.tokenResponse.credential;
 
     return (
-        <>
-        {console.log(state)}
-        {console.log(state.credential)}
+        <>{console.log(state)}
+        {console.log(state.tokenResponse.credential)}
         <h1>Hello {access_token}</h1>
             
         </>
