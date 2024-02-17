ca1b5d91109a7114b54ad52fcc3ec86bea90fcf8
atharva-jadhav05 <160360862+atharva-jadhav05@users.noreply.github.com>
2024-02-18 04:01:01 +0530

    Update dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from 'react-router-dom';
 
-const Dashboard = ({location}) => {
+const Dashboard = () => {
     const location = useLocation();
     const token = location && location.state ? location.state : null;
 
