4043e97fedd5ff66f87bf73298651d3e538d6397
Asmita Jadhav <asmita.jadhav@cumminscollege.in>
2024-03-07 01:13:37 +0530

    workspace: fetch files from drive

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
 
 
@@ -9,6 +10,27 @@ const Workspace = () => {
     const folderName = state?.name || null;
     const folderId = state?.id || null;
     const accessToken = state?.accessToken || null; 
+
+    const [files, setFiles] = useState([]);
+
+    useEffect(() => {
+        const apiUrl = 'https://www.googleapis.com/drive/v3/files';
+
+        const response = axios.get(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+            params: {
+                q: `'${folderId}' in parents and mimeType='application/pdf'`,
+            },
+        });
+
+        const pdf_files = response.data.files;
+        setFiles(pdf_files)
+        console.log(pdf_files);
+
+    });
+
 
 
     return (
