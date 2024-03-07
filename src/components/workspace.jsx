7f3e8e9346a134fc1caaee50f75f2277ca69616d
Asmita Jadhav <asmita.jadhav@cumminscollege.in>
2024-03-07 11:27:21 +0530

    workspace: bookmark button css

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -114,8 +114,13 @@ const Workspace = () => {
     };
 
     const addBookmark = () => {
-        setBookmarks([...bookmarks, { fileId: currentFileId, name: bookmark_name.current.value, page: bookmark_page.current.value }]);
-        setToShowBookmarks([...toShowBookmarks, { fileId: currentFileId, name: bookmark_name.current.value, page: bookmark_page.current.value }]);
+        const currentUrl = iframeRef.current.src;
+        const basePdfUrl = currentUrl.split('#')[0];
+
+        const url = `${basePdfUrl}#page=${bookmark.page}`;
+
+        setBookmarks([...bookmarks, { fileId: currentFileId, name: bookmark_name.current.value, page: bookmark_page.current.value, url: url }]);
+        setToShowBookmarks([...toShowBookmarks, { fileId: currentFileId, name: bookmark_name.current.value, page: bookmark_page.current.value, url: url  }]);
         console.log(currentFileId, bookmark_name.current.value, bookmark_page.current.value);
         bookmark_name.current.value = '';
         bookmark_page.current.value = '';
@@ -192,10 +197,11 @@ const Workspace = () => {
 
 
     const handleBookmarkClick = (bookmark) => {
-        const currentUrl = iframeRef.current.src;
-        const basePdfUrl = currentUrl.split('#')[0];
+        // const currentUrl = iframeRef.current.src;
+        // const basePdfUrl = currentUrl.split('#')[0];
 
-        iframeRef.current.src = `${basePdfUrl}#page=${bookmark.page}`;
+        // iframeRef.current.src = `${basePdfUrl}#page=${bookmark.page}`;
+        iframeRef.current.src = bookmark.url;
         iframeRef.current.contentWindow.location.reload(true);
 
     }
