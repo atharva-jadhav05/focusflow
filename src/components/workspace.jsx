c69714357140438c874c92573f645dca3be44417
Asmita Jadhav <asmita.jadhav@cumminscollege.in>
2024-03-07 11:38:58 +0530

    workspace: bookmark button css

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -203,7 +203,7 @@ const Workspace = () => {
         // iframeRef.current.src = `${basePdfUrl}#page=${bookmark.page}`;
         iframeRef.current.src = bookmark.url;
         console.log(iframeRef.current.src);
-        iframeRef.current.contentWindow.location.reload(true);
+        // iframeRef.current.contentWindow.location.reload(true);
 
     }
 
