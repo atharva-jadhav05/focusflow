57a358f6ad577c27bfc157902f8400ceec1fa956
Asmita Jadhav <asmita.jadhav@cumminscollege.in>
2024-03-15 23:25:39 +0530

    Logout

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
+import { GoogleLogin, useGoogleLogout } from '@react-oauth/google';
 import axios from 'axios';
 
 import './dashboard.css';
